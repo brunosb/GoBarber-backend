09bfe0dfe3197e5902953ab055aa1e10b25a0b35
Bruno Barbosa <barbosa.developer@gmail.com>
2020-04-14 20:41:27 -0300

    Banco de dados; Type ORM; Tabela Appointments; Tabela Usuario; Migrations; Relacionamento nos models; Bcrypt; Autenticação de usuário; Authenticação de rotas; JWT

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e2520fa2584a8746eca028e8b83fce95',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
+
+// SoC: Separations of Concerns (Separação de preocupações)
+// DTO - Data Transfer Object
+
+// Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
+
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider_id, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService();
+
+    const appointment = await createAppointmentService.execute({
+      date: parsedDate,
+      provider_id,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRouter;
